5c5c92e9056193e5cdc190e9d790b91150fbdc5a
Ter <114605209+UnioTrias@users.noreply.github.com>
2023-06-16 15:49:26 +0200

    ajuste

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,24 +177,6 @@
   }
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
